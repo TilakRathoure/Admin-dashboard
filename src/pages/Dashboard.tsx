74fc309a66fda1f55f4fd6ebd0bb667ab96c111b
Tilak Rathoure <tilakrathoure@gmail.com>
2024-09-30 21:31:21 +0530

    add Inventory

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import { BsSearch} from "react-icons/bs";
 import { FaRegBell } from "react-icons/fa";
 import Image1 from "../assets/user.png";
 import { HiTrendingDown,HiTrendingUp } from "react-icons/hi";
+import data from "../assets/data.json";
 
 const Dashboard = () => {
 
@@ -25,7 +26,10 @@ const Dashboard = () => {
 
       <div className="w-1/4 h-[100vh]">
 
+
       <AdminSidebar/>
+
+
 
       </div>
 
@@ -35,7 +39,7 @@ const Dashboard = () => {
       <div className="flex gap-2 justify-between">
         {DashElements.map((elements)=>(
 
-          <div className="flex bg-white w-[13rem] h-[8rem] rounded-xl p-4 justify-between">
+          <div className="flex bg-white w-[16rem] h-[8rem] rounded-xl p-4 justify-around">
 
             <div className="flex flex-col ">
             <h1>{elements.heading}</h1>
@@ -43,8 +47,6 @@ const Dashboard = () => {
             {elements.change>0 ? <p className="flex items-center gap-1"><HiTrendingUp className="text-green-500"/><span className="text-green-500">+{elements.change}</span></p>:<p className="flex items-center gap-1"><HiTrendingDown className="text-red-500"/><span className="text-red-500">{elements.change}</span></p>}
 
             </div>
-
-
 
             <div className="flex items-center justify-center">
             <div className="flex-shrink-0 flex justify-center items-center h-[5rem] w-[5rem] rounded-full" style={{background: `conic-gradient(${elements.color} 0deg ${Math.abs(elements.graph) / 100 * 360}deg, transparent 0deg)`}}>
@@ -56,6 +58,50 @@ const Dashboard = () => {
               </div>
 
         ))}
+      </div>
+
+      {/* Chart and invertory */}
+
+      {/* chart */}
+
+
+      <div className="w-full flex gap-7">
+
+        <div className="w-3/4 bg-white rounded-xl">
+
+        </div>
+
+
+        {/* INVENTORY */}
+
+        <div className="w-[23.5%] bg-white rounded-lg p-5 flex flex-col gap-6">
+
+        <h1 className="text-2xl opacity-60 uppercase text-center">Inventory</h1>
+
+        <div className="flex flex-col gap-4">
+
+        {data.inventory.map((elements)=>(
+
+          <div>
+
+            <div className="flex items-center justify-evenly gap-2">
+              <div className="">{elements.heading}</div>
+              <div className="w-[95px] bg-gray-200 rounded-xl h-[15px]"><div className="bg-green-400 rounded-xl h-[15px]" style={{ width: `${elements.value}%` }}></div></div>
+              <div>{elements.value}</div>
+
+
+            </div>
+
+            </div>
+
+            
+
+        ))}
+
+        </div>
+
+        </div>
+
       </div>
 
       
